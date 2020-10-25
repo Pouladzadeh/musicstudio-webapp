df1f22affde4936ef7d7db2b5fcf7151bc0603aa
Parvaneh <parvaneh.pouladzadeh@gmail.com>
2020-10-24 23:22:58 -0400

    social media icons added on homepage.

--- scripts.js
+++ scripts.js
@@ -34,19 +34,19 @@ $(document).ready(function () {
   });
 
   // typing text animation script
-  var typed = new Typed(".typing", {
-    strings: ["Nastaran Music Studio"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
+  // var typed = new Typed(".typing", {
+  //   strings: ["Nastaran Music Studio"],
+  //   typeSpeed: 100,
+  //   backSpeed: 60,
+  //   loop: true,
+  // });
 
-  var typed = new Typed(".typing-2", {
-    strings: ["Music Instructor"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
+  // var typed = new Typed(".typing-2", {
+  //   strings: ["Music Instructor"],
+  //   typeSpeed: 100,
+  //   backSpeed: 60,
+  //   loop: true,
+  // });
 
   // owl carousel script
   $(".carousel").owlCarousel({
